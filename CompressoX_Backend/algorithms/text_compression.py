1e004e2c5949b331f8052f97ac3f033083f74384
LokendraPandey1 <160410746+LokendraPandey1@users.noreply.github.com>
2025-05-20 23:42:05 +0530

    Delete CompressoX_Backend directory

--- CompressoX_Backend/algorithms/text_compression.py
+++
@@ -1,9 +0,0 @@
-from .text_lossy_compression import compress_text_lossy
-from .text_lossless_compression import compress_text_lossless
-
-def compress_text(input_path: str, output_path: str, is_lossy: bool = True) -> dict:
-    """Compress a text file using either lossy or lossless compression"""
-    if is_lossy:
-        return compress_text_lossy(input_path, output_path)
-    else:
-        return compress_text_lossless(input_path, output_path)
